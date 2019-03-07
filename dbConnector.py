4bee1f968f9e22c1ca19f35c0f7cdf640b88c15c
gramateusz123 <gramateusz1@gmail.com>
2019-03-07 19:19:58 +0100

    push

---
+++ dbConnector.py
@@ -0,0 +1,19 @@
+import mysql.connector
+data_bases = []
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="admin",
+  passwd="admin"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  data_bases.append(x)
+
+for x in data_bases:
+  print(x)
+  
+if "mysql" in data_bases:
+  print("This library already exist!")
