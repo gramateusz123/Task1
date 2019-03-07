91e793d6987a21e684ded1e4b395fb396c5a38e4
gramateusz123 <gramateusz1@gmail.com>
2019-03-07 21:27:14 +0100

    pusham bo będę modyfikował

--- dbConnector.py
+++ dbConnector.py
@@ -31,4 +31,21 @@ else:
   )
   mycursor = mydb.cursor()
   mycursor.execute("CREATE TABLE books (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), author VARCHAR(255))")
-print("Welcome to the Library database!" /n "What do you want to do?" /n "1. Show book list." /n "2. Add new book.")
+  print("Welcome to the Library database!")
+  print("What do you want to do?")
+  print("1. Show book list.")
+  print("2. Add new book.")
+  
+decision1 = input()
+decision1 = int(decision1)
+
+if decision1==2:
+  print("What is the title?:")
+  title = input()
+  print("Who is an author ?:")
+  author = input()
+  sql = "INSERT INTO books (title, author) VALUES (%s, %s)"
+  val = (title, author)
+  mycursor.execute(sql, val)
+
+  mydb.commit()
