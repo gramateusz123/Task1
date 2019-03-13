b5ad1d8a4f92d8cea02413edba39a70f4af48516
gramateusz123 <gramateusz1@gmail.com>
2019-03-13 20:15:19 +0100

    matkie boskie

--- dbConnector.py
+++ dbConnector.py
@@ -1,5 +1,8 @@
 import mysql.connector
 data_bases = []
+from func import text
+from func import end
+close = 0
 mydb = mysql.connector.connect(
   host="localhost",
   user="admin",
@@ -15,55 +18,41 @@ for x in mycursor:
 
 if "('library',)" not in data_bases:
   mycursor.execute("CREATE DATABASE library")
-  
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="admin",
-    passwd="admin",
-    database="library"
-  )
-  mycursor = mydb.cursor()
   mycursor.execute("CREATE TABLE books (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), author VARCHAR(255))")
 
-  
-print("Welcome to the Library database!")
-print("What do you want to do?")
-print("1. Show book list.")
-print("2. Add new book.")
-  
-decision1 = input()
-decision1 = int(decision1)
-
-if decision1==1:
-  mydb = mysql.connector.connect(
-  host="localhost",
-  user="admin",
-  passwd="admin",
-  database="library"
-)
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM books")
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
-
-if decision1==2:
-  print("What is the title?:")
-  title = input()
-  print("Who is an author ?:")
-  author = input()
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="admin",
-    passwd="admin",
-    database="library"
-  )
+while close == 0:
+  text()
+  decision1 = input()
+  mydb = mysql.connector.connect(                                      #nie mogę wrzucić tego w funkcję, bo wypierdala błąd, że nie jest wskazany database
+      host="localhost",
+      user="admin",
+      passwd="admin",
+      database="library"
+      )
   mycursor = mydb.cursor()
-  sql = "INSERT INTO books (title, author) VALUES (%s, %s)"
-  val = (title, author)
-  mycursor.execute(sql, val)
-
-  mydb.commit()
-
-  print(mycursor.rowcount, "Book successfully added!")
+  
+  if decision1=="1":
+    mycursor.execute("SELECT * FROM books")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+      print(x)
+    end()
+  
+  elif decision1=="2":
+    print("What is the title?:")
+    title = input()
+    print("Who is an author ?:")
+    author = input()
+    sql = "INSERT INTO books (title, author) VALUES (%s, %s)"
+    val = (title, author)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "Book successfully added!")
+    end()
+  
+  elif decision1=="3":
+    quit()
+  
+  else:
+    print("Choose an option!")
+    close = 0
